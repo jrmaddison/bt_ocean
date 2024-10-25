6e2f7c7e15a6590b9f57ed8ce449345f5d149b88
James R. Maddison <j.r.maddison@ed.ac.uk>
2024-10-25 14:11:04 +0100

    Reorder

--- tests/test_base.py
+++ tests/test_base.py
@@ -15,13 +15,13 @@ __all__ = \
 @pytest.fixture(autouse=True, scope="module",
                 params=[{"x64_enabled": False}, {"x64_enabled": True}])
 def test_precision(request):
-    floatx = keras.backend.floatx()
     x64_enabled = jax.config.x64_enabled
-    keras.backend.set_floatx("float64" if request.param["x64_enabled"] else "float32")
     jax.config.update("jax_enable_x64", request.param["x64_enabled"])
+    floatx = keras.backend.floatx()
+    keras.backend.set_floatx("float64" if request.param["x64_enabled"] else "float32")
     yield
-    keras.backend.set_floatx(floatx)
     jax.config.update("jax_enable_x64", x64_enabled)
+    keras.backend.set_floatx(floatx)
 
 
 def eps():
