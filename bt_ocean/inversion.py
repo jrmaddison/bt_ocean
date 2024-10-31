3beefabad9b4c8bcc3ae4ce367cb729bb53a52ed
James R. Maddison <j.r.maddison@ed.ac.uk>
2024-10-31 15:22:55 +0000

    DST-based finite difference modified Helmholtz solver

--- bt_ocean/inversion.py
+++ bt_ocean/inversion.py
@@ -1,109 +1,53 @@
 """Linear solvers.
 """
 
-import jax
 import jax.numpy as jnp
-from jax.scipy.linalg import lu_factor, lu_solve
-import numpy as np
 
-from functools import cached_property
-
+from .fft import dst, idst
 from .grid import Grid
 
 
 __all__ = \
     [
-        "KroneckerProductSolver",
         "ModifiedHelmholtzSolver",
         "PoissonSolver"
     ]
 
 
-class KroneckerProductSolver:
-    r"""Solver for a linear system with Kronecker product structure,
+class ModifiedHelmholtzSolver:
+    r"""Solver for the 2D modified Helmholtz equation,
 
     .. math::
 
-        ( I \otimes A + B \otimes I ) u = -b,
+        ( \beta ( \partial_{xx} + \partial_{yy} ) - \alpha  ) u = b,
 
-    using equation (3.9) of
-
-        - Robert E. Lynch, John R. Rice and Donald H. Thomas, 'Direct solution
-          of partial difference equations by tensor product methods',
-          Numerische Mathematik 6, pp. 185--199, 1964,
-          https://doi.org/10.1007/BF01386067
-
-    Warnings
-    --------
-
-    Note the extra negative sign in the definition of the right-hand-side.
+    subject to homogeneous Dirichlet boundary conditions, using a finite
+    difference discretization.
 
     Parameters
     ----------
 
-    A : :class:`jax.Array`
-        Defines the matrix :math:`A`.
-    B : :class:`jax.Array`
-        Defines the matrix :math:`B`.
+    grid : :class:`.Grid`
+        The 2D grid.
+    alpha : Real
+        :math:`\alpha`.
+    beta : Real
+        :math:`\beta`.
     """
 
-    def __init__(self, A, B):
-        self._A = A
-        self._B = B
+    def __init__(self, grid, alpha, beta=1):
+        self._grid = grid
+        self._alpha = grid.fdtype(alpha)
+        self._beta = grid.fdtype(beta)
 
-        # Cached property initialization
-        self._A_decomp
-        self._B_decomp
+    @property
+    def grid(self) -> Grid:
+        """The 2D grid."""
 
-    @staticmethod
-    def _decomp(A):
-        if isinstance(A, jax._src.core.Tracer):
-            with jax.default_device(jax.devices("cpu")[0]):
-                Lam, V = jnp.linalg.eig(A)
-                Lam = Lam.real
-                V = V.real
-        else:
-            Lam, V = np.linalg.eig(np.array(A, dtype=A.dtype))
-            assert (Lam.imag == 0).all()
-            assert (V.imag == 0).all()
-            Lam = jnp.array(Lam.real, dtype=A.dtype)
-            V = jnp.array(V.real, dtype=A.dtype)
-
-        return Lam, V, lu_factor(V)
-
-    @cached_property
-    def _A_decomp(self):
-        return self._decomp(self._A)
-
-    @cached_property
-    def _B_decomp(self):
-        return self._decomp(self._B)
-
-    @staticmethod
-    @jax.jit
-    def _solve(Lam_A, Q, Q_lu, Lam_B, P, P_lu, b):
-        # Linear solve using equation (3.9) of
-        #     Robert E. Lynch, John R. Rice and Donald H. Thomas, 'Direct
-        #     solution of partial difference equations by tensor product
-        #     methods', Numerische Mathematik 6, pp. 185--199, 1964,
-        #     https://doi.org/10.1007/BF01386067
-
-        u = lu_solve(Q_lu, -b.T).T
-        u = lu_solve(P_lu, u)
-        scale = (jnp.outer(jnp.ones_like(u, shape=(u.shape[0],)), Lam_A)
-                 + jnp.outer(Lam_B, jnp.ones_like(u, shape=(u.shape[1],))))
-        u = P @ (u / scale) @ Q.T
-        return u
+        return self._grid
 
     def solve(self, b):
         """Solve the linear system.
-
-        Computed using equation (3.9) of
-
-            - Robert E. Lynch, John R. Rice and Donald H. Thomas, 'Direct
-              solution of partial difference equations by tensor product
-              methods', Numerische Mathematik 6, pp. 185--199, 1964,
-              https://doi.org/10.1007/BF01386067
 
         Parameters
         ----------
@@ -119,45 +63,14 @@ class KroneckerProductSolver:
             The solution :math:`u`. An ndim 2 array.
         """
 
-        return self._solve(*(self._A_decomp + self._B_decomp + (b,)))
+        b_tilde = dst(dst(b, axis=0), axis=1)
+        a_k = 2 * (jnp.cos(jnp.arange(1, self.grid.N_x, dtype=self.grid.fdtype) * jnp.pi / self.grid.N_x) - 1) * ((self.grid.N_x / (2 * self.grid.L_x)) ** 2)
+        a_l = 2 * (jnp.cos(jnp.arange(1, self.grid.N_y, dtype=self.grid.fdtype) * jnp.pi / self.grid.N_y) - 1) * ((self.grid.N_y / (2 * self.grid.L_y)) ** 2)
+        u_tilde = jnp.zeros_like(b).at[1:-1, 1:-1].set(
+            -b_tilde[1:-1, 1:-1] / (self._alpha - self._beta * (jnp.outer(a_k, jnp.ones_like(a_l)) + jnp.outer(jnp.ones_like(a_k), a_l))))
+        u = idst(idst(u_tilde, axis=0), axis=1)
 
-
-class ModifiedHelmholtzSolver(KroneckerProductSolver):
-    r"""Solver for the 2D modified Helmholtz equation,
-
-    .. math::
-
-        ( \alpha - \beta ( \partial_{xx} - \partial_{yy} ) ) u = -b,
-
-    subject to homogeneous Dirichlet boundary conditions, using a Chebyshev
-    pseudospectral discretization.
-
-    Parameters
-    ----------
-
-    grid : :class:`.Grid`
-        The 2D Chebyshev grid.
-    alpha : Real
-        :math:`\alpha`.
-    beta : Real
-        :math:`\beta`.
-    """
-
-    def __init__(self, grid, alpha, beta=1):
-        A = 0.5 * alpha * jnp.eye(grid.N_y - 1, dtype=grid.fdtype) - beta * grid.D_yy[1:-1, 1:-1]
-        B = 0.5 * alpha * jnp.eye(grid.N_x - 1, dtype=grid.fdtype) - beta * grid.D_xx[1:-1, 1:-1]
-
-        super().__init__(A, B)
-        self._grid = grid
-
-    @property
-    def grid(self) -> Grid:
-        """The 2D Chebyshev grid."""
-
-        return self._grid
-
-    def solve(self, b):
-        return jnp.zeros_like(b).at[1:-1, 1:-1].set(super().solve(b[1:-1, 1:-1]))
+        return u
 
 
 class PoissonSolver(ModifiedHelmholtzSolver):
@@ -165,16 +78,16 @@ class PoissonSolver(ModifiedHelmholtzSolver):
 
     .. math::
 
-        -( \partial_{xx} + \partial_{yy} ) u = -b,
+        ( \partial_{xx} + \partial_{yy} ) u = b,
 
-    subject to homogeneous Dirichlet boundary conditions, using a Chebyshev
-    pseudospectral discretization.
+    subject to homogeneous Dirichlet boundary conditions, using a finite
+    difference discretization.
 
     Parameters
     ----------
 
     grid : :class:`.Grid`
-        The 2D Chebyshev grid.
+        The 2D grid.
     """
 
     def __init__(self, grid):
--- tests/test_inverse.py
+++ tests/test_inverse.py
@@ -1,7 +1,6 @@
 from bt_ocean.grid import Grid
 from bt_ocean.inversion import ModifiedHelmholtzSolver, PoissonSolver
 
-import jax
 import jax.numpy as jnp
 from numpy import cbrt, exp
 import pytest
@@ -21,29 +20,12 @@ def test_poisson_solver_residual(L_x, L_y, N_x, N_y):
 
     X, Y = solver.grid.X, solver.grid.Y
     b_ref = (jnp.sin((jnp.pi * (X + L_x)) / (2 * L_x))
-             * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y)))
+             * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y))
+             * jnp.exp(X) * jnp.exp(Y))
     u = solver.solve(b_ref)
 
-    b = (solver.grid.D_xx @ u + u @ solver.grid.D_yy.T)[1:-1, 1:-1]
+    b = (solver.grid.D_xx(u) + solver.grid.D_yy(u))[1:-1, 1:-1]
     error_norm = abs(b - b_ref[1:-1, 1:-1]).max() / abs(b).max()
-    print(f"{error_norm=}")
-    assert error_norm < (1.0e3, 1.0e4)[int(jax.config.x64_enabled)] * eps()
-
-
-@pytest.mark.parametrize("L_x, L_y", [(1, 1),
-                                      (cbrt(3), cbrt(2))])
-@pytest.mark.parametrize("N_x, N_y", [(50, 50),
-                                      (50, 100),
-                                      (100, 50)])
-def test_poisson_solver_convergence(L_x, L_y, N_x, N_y):
-    solver = PoissonSolver(Grid(L_x, L_y, N_x, N_y))
-
-    X, Y = solver.grid.X, solver.grid.Y
-    b = (jnp.sin((jnp.pi * (X + L_x)) / (2 * L_x))
-         * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y)))
-    u = solver.solve(b)
-    u_ref = -b / ((jnp.pi ** 2) * (1 / (2 * L_x) ** 2 + 9 / ((2 * L_y) ** 2)))
-    error_norm = abs(u - u_ref).max()
     print(f"{error_norm=}")
     assert error_norm < 1.0e3 * eps()
 
@@ -61,30 +43,11 @@ def test_modified_helmholtz_solver_residual(L_x, L_y, N_x, N_y, alpha):
 
     X, Y = solver.grid.X, solver.grid.Y
     b_ref = (jnp.sin((jnp.pi * (X + L_x)) / (2 * L_x))
-             * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y)))
+             * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y))
+             * jnp.exp(X) * jnp.exp(Y))
     u = solver.solve(b_ref)
 
-    b = (solver.grid.D_xx @ u + u @ solver.grid.D_yy.T - alpha * u)[1:-1, 1:-1]
+    b = (solver.grid.D_xx(u) + solver.grid.D_yy(u) - alpha * u)[1:-1, 1:-1]
     error_norm = abs(b - b_ref[1:-1, 1:-1]).max() / abs(b).max()
     print(f"{error_norm=}")
-    assert error_norm < (1.0e3, 1.0e4)[int(jax.config.x64_enabled)] * eps()
-
-
-@pytest.mark.parametrize("L_x, L_y", [(1, 1),
-                                      (cbrt(3), cbrt(2))])
-@pytest.mark.parametrize("N_x, N_y", [(50, 50),
-                                      (50, 100),
-                                      (100, 50)])
-@pytest.mark.parametrize("alpha", (0.5, 1, 2, exp(-0.5)))
-def test_modified_helmholtz_solver_convergence(L_x, L_y, N_x, N_y, alpha):
-    solver = ModifiedHelmholtzSolver(
-        Grid(L_x, L_y, N_x, N_y), alpha=alpha)
-
-    X, Y = solver.grid.X, solver.grid.Y
-    b = (jnp.sin((jnp.pi * (X + L_x)) / (2 * L_x))
-         * jnp.sin((3 * jnp.pi * (Y + L_y)) / (2 * L_y)))
-    u = solver.solve(b)
-    u_ref = -b / ((jnp.pi ** 2) * (1 / (2 * L_x) ** 2 + 9 / ((2 * L_y) ** 2)) + alpha)
-    error_norm = abs(u - u_ref).max()
-    print(f"{error_norm=}")
-    assert error_norm < 100 * eps()
+    assert error_norm < 1.0e3 * eps()
